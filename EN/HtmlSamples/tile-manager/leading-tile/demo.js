ce64f8f3c6dbc3bccbdf2ca1e6c80f8f55e8b5e2
unknown <SYSTEM@S0106WEB3S.IGWEB.LOCAL>
2013-07-10 10:49:59 -0400

    SB build 13.2.20132.436

--- EN/HtmlSamples/tile-manager/leading-tile/demo.js
+++ EN/HtmlSamples/tile-manager/leading-tile/demo.js
@@ -1,35 +1,6 @@
 $(function () {
-var activated = [false, false, false, false]
-        $(function () {
-            $('#car-tabs').tabs({
-                beforeActivate: function (event, ui) {
-                    var index = ui.newTab.index();
-                    if (!activated[index]) {
-                        ui.newPanel.igTileManager({
-                            layoutConfiguration: {
-                                gridLayout: {
-                                    columnWidth: 230,
-                                    columnHeight: 210,
-                                    marginLeft: 10,
-                                    marginTop: 10
-                                },
-                                items: [
-                                    { rowIndex: 0, colIndex: 0, rowSpan: 2, colSpan: 2 },
-                                    { rowIndex: 0, colIndex: 2, rowSpan: 1, colSpan: 1 },
-                                    { rowIndex: 1, colIndex: 2, rowSpan: 1, colSpan: 1 },
-                                    { rowIndex: 2, colIndex: 0, rowSpan: 1, colSpan: 1 },
-                                    { rowIndex: 2, colIndex: 1, rowSpan: 1, colSpan: 1 },
-                                    { rowIndex: 2, colIndex: 2, rowSpan: 1, colSpan: 1 }
-                                ]
-                            },
-                            maximizedTileIndex: 0,
-                            minimizedState: ':not(ul)'
-                        });
-                        activated[index] = true;
-                    }
-                }
-            });
-            $('#mercedes').igTileManager({
+var activated = [false, false, false, false],
+            options = {
                 layoutConfiguration: {
                     gridLayout: {
                         columnWidth: 230,
@@ -48,7 +19,18 @@ var activated = [false, false, false, false]
                 },
                 maximizedTileIndex: 0,
                 minimizedState: ':not(ul)'
+            };
+        $(function () {
+            $('#car-tabs').tabs({
+                beforeActivate: function (event, ui) {
+                    var index = ui.newTab.index();
+                    if (!activated[index]) {
+                        ui.newPanel.igTileManager(options);
+                        activated[index] = true;
+                    }
+                }
             });
+            $('#mercedes').igTileManager(options);
             activated[0] = true;
         });
 });

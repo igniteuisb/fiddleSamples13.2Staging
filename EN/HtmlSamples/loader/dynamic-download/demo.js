c782be959c32f0a8175a3d9cce14e59207085f39
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-08-27 13:05:32 -0400

    SB build 13.2.20132.874

--- EN/HtmlSamples/loader/dynamic-download/demo.js
+++ EN/HtmlSamples/loader/dynamic-download/demo.js
@@ -1,7 +1,7 @@
 $(function () {
 $.ig.loader({
-            scriptPath: "http://igniteuisamples.staging.infragistics.local/13-2/IgniteUI/js/",
-            cssPath: "http://igniteuisamples.staging.infragistics.local/13-2/IgniteUI/css/",
+            scriptPath: "http://staging.igniteui.local/13-2/IgniteUI/js/",
+            cssPath: "http://staging.igniteui.local/13-2/IgniteUI/css/",
             resources: "igShared",
             ready: function () {
                 //This button click triggers the tree to be created

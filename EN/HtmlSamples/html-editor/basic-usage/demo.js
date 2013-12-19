da2e06558cab2fcf4cd78bdabcd63d1632c7acd7
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2013-12-19 08:28:30 -0500

    SB build 13.2.20132.1913

--- EN/HtmlSamples/html-editor/basic-usage/demo.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-            var height = $('html').hasClass('touch') ? 500 : 350;
-
-            $("#htmlEditor").igHtmlEditor({ height: height });
-        });
--- EN/HtmlSamples/tile-manager/markup/demo.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-            $('#dashboard').igTileManager({
-                columnWidth: 250,
-                columnHeight: 250,
-                marginLeft: 10,
-                marginTop: 10
-            });
-        });
